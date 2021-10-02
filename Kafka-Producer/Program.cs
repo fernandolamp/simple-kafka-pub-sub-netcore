1e3eadc0f79c37aadf865290f1d124fc79794c3a
Fernando Gutierres Damaceno <fernando.damaceno@softplan.com.br>
2021-10-02 15:40:54 -0300

    feat: kafka pub sub

---
+++ Kafka-Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Bogus;
+using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+
+namespace Kafka_Producer
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var faker = new Faker();
+            using var p = new ProducerBuilder<int, string>(new ProducerConfig { BootstrapServers = "localhost:29092" }).Build();
+
+            while (true)
+            {
+                Console.WriteLine("Click any key to generate a random value.");
+                Console.ReadKey();
+
+                try
+                {                    
+                    var dr = await p.ProduceAsync("cabeloTopic", new Message<int, string> { Key = 1, Value = faker.Company.Bs() });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
---
+++ kafka-consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace kafka_consumer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group-1",
+                BootstrapServers = "localhost:29092",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnableAutoCommit = true,
+                EnableAutoOffsetStore = false
+            };
+
+            var consumerBuilder = new ConsumerBuilder<Ignore, string>(conf);
+            using var c = consumerBuilder.Build();
+            c.Subscribe("cabeloTopic");
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var cr = c.Consume();
+                        Console.WriteLine($"Consumed message '{cr.Message.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        c.StoreOffset(cr);
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                c.Close();
+            }
+        }
+    }
+}
